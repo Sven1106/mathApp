0b45cfcc523eb54d1d7525ec8366fec791929b84
Svend Aage Roperos Nielsen <sven1106@VID.net.local>
2018-09-25 21:43:47 +0200

    changes to getNearestDivisible()

--- src/pages/calculate/calculate.ts
+++ src/pages/calculate/calculate.ts
@@ -15,6 +15,9 @@ import * as RandomJs from 'random-js';
 export class CalculatePage {
   helper = new Helper();
   randomJs = new RandomJs();
+  blah: number = 0;
+  rndInt1;
+  rndInt2;
   constructor(private nativePageTransitions: NativePageTransitions, public navCtrl: NavController, public navParams: NavParams, public mathService: MathService) {
 
   }
@@ -23,8 +26,8 @@ export class CalculatePage {
     let mathTypes: IMathType[] = this.mathService.getMathTypes.filter(e => e.isSelected == true).map(e => e);
     let randomMathType = mathTypes[this.randomJs.integer(0, mathTypes.length - 1)];
 
-    let rndInt1 = this.getRndInputInt(this.mathService.input1);
-    let rndInt2 = this.getRndInputInt(this.mathService.input2);
+    this.rndInt1 = this.getRndInputInt(this.mathService.input1);
+    this.rndInt2 = this.getRndInputInt(this.mathService.input2);
     // let rnd = {
     //   input1: this.getRndInputInt(this.mathService.input1),
     //   input2: this.getRndInputInt(this.mathService.input2)
@@ -36,27 +39,27 @@ export class CalculatePage {
     switch (randomMathType.title) {
       case 'Addition':
       case 'Multiplication':
-        console.log(rndInt1 + ' ' + randomMathType.operator + ' ' + rndInt2);
+        console.log(this.rndInt1 + ' ' + randomMathType.operator + ' ' + this.rndInt2);
         break;
 
       case 'Subtraction':
-        if (rndInt2 > rndInt1) {
-          console.log(rndInt2 + ' ' + randomMathType.operator + ' ' + rndInt1);
+        if (this.rndInt2 > this.rndInt1) {
+          console.log(this.rndInt2 + ' ' + randomMathType.operator + ' ' + this.rndInt1);
         }
         else {
-          console.log(rndInt1 + ' ' + randomMathType.operator + ' ' + rndInt2);
+          console.log(this.rndInt1 + ' ' + randomMathType.operator + ' ' + this.rndInt2);
         }
         break;
 
       case 'Division':
-        console.log(this.isPrime(rndInt2));
-        console.log(rndInt1 + '/' + rndInt2)
-        // if (rndInt2 > rndInt1) {
-        //   this.getNearestDivisible(rndInt2, rndInt1);
-        // }
-        // else {
-        //   this.getNearestDivisible(rndInt1, rndInt2);
-        // }
+        // console.log(this.isPrime(rndInt2));
+        // console.log(rndInt1 + '/' + rndInt2)
+        if (this.rndInt2 > this.rndInt1) {
+          this.getNearestDivisible(this.rndInt2, this.rndInt1);
+        }
+        else {
+          this.getNearestDivisible(this.rndInt1, this.rndInt2);
+        }
         break;
       default:
         alert('Incorrect mathType was provided');
@@ -77,7 +80,7 @@ export class CalculatePage {
 
   getRndInputInt(input) {
     let inputRange = this.helper.getDigitRange(input);
-    return this.randomJs.integer(inputRange.smallest, inputRange.biggest);
+    return this.helper.getRndInt(inputRange.smallest, inputRange.biggest);
   }
 
   getNearestDivisible(x, y) {
@@ -88,7 +91,24 @@ export class CalculatePage {
         this.getNearestDivisible(x, y - 1);
       }
       else {
-        return console.log(x - remainder + '/' + y);
+
+        if (x - remainder != y) { // if x and y isnt equal
+          console.log('remove remainder: (' + x + '-' + remainder + ')/' + y);
+          return console.log(x - remainder + '/' + y);
+        }
+        else {
+          console.log('doing magic because: '+ (x - remainder) +'/' + y);
+          this.rndInt1 = this.getRndInputInt(this.mathService.input1);
+          this.rndInt2 = this.getRndInputInt(this.mathService.input2);
+          if (this.rndInt2 > this.rndInt1) {
+            this.getNearestDivisible(this.rndInt2, this.rndInt1);
+          }
+          else {
+            this.getNearestDivisible(this.rndInt1, this.rndInt2);
+          }
+        }
+
+
       }
     }
     else {
